54602e1bf558d169f22de07326795a0f0dc1284f
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-08-09 11:41:58 +0200

    App.test created

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable testing-library/no-node-access */
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("<App /> component", () => {
+  let AppDOM;
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    AppDOM = render(<App />).container.firstChild;
+  });
+  test("renders list of events", () => {
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+  });
+  test("render CitySearch", () => {
+    expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
+  });
+});
