7fc7d8350053f9b9303835b75af332cf60b6490a
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-08-09 14:08:33 +0200

    show event details  test is added

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,26 @@
-const Event = () => {
-  return <li></li>;
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const handleClick = () => {
+    setShowDetails(!showDetails);
+  };
+  return (
+    <li>
+      <h3>{event.summary}</h3>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      <button onClick={handleClick}>
+        {showDetails ? "Hide Details" : "Show Details"}
+      </button>
+      {showDetails ? (
+        <div className="details">
+          <b>Event Details</b>
+          <p>Description: {event.description}</p>
+          <p>Event status: {event.status}</p>
+        </div>
+      ) : null}
+    </li>
+  );
 };
 export default Event;
